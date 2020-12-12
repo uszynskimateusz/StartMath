3b9fb01e108bfe0a441f2c4b5f9e389eb5d0580d
uszynskimateusz <uszynskimateusz@gmail.com>
2020-12-12 15:00:17 +0100

    Initial Commit

---
+++ StartMath/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StartMath
+//
+//  Created by Mateusz Uszyński on 12/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
