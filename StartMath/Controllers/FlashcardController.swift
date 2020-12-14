c952af9d45bf796b2aa20d9f5aead8db77d3ef83
uszynskimateusz <uszynskimateusz@gmail.com>
2020-12-14 14:32:49 +0100

    flashcard view added

---
+++ StartMath/Controllers/FlashcardController.swift
@@ -0,0 +1,18 @@
+//
+//  FlashcardController.swift
+//  StartMath
+//
+//  Created by Mateusz Uszyński on 14/12/2020.
+//
+
+import UIKit
+
+class FlashcardController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
